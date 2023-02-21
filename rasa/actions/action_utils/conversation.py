536d0577e482508427bcd85feb408ba13233b239
roger <climberwoodi@gmx.ch>
2023-02-21 16:23:14 +0100

    Added cleaning of messages. Removed all markdown code.

--- rasa/actions/action_utils/conversation.py
+++ rasa/actions/action_utils/conversation.py
@@ -1,10 +1,19 @@
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+
+def _get_cleaned_text(text):
+    html = markdown(text)
+    return ' '.join(BeautifulSoup(html).findAll(text=True))
+
+
 def get_last_conversation(events, max_number_of_last_conversations):
     events_cleaned = sorted(events, key=lambda x: x['timestamp'], reverse=True)
     events_cleaned = list(filter(lambda x: x['event'] in ['user', 'bot'], events_cleaned))
     conversation = []
-    for event in events_cleaned[0:5]:
+    for event in events_cleaned[0:max_number_of_last_conversations]:
         if event['event'] == 'user':
-            conversation.append("Benutzer: " + event['text'] + "\n")
+            conversation.append("Benutzer: " + _get_cleaned_text(event['text']) + "\n")
         elif event['event'] == 'bot':
-            conversation.append("Chatbot: " + event['text'] + "\n")
+            conversation.append("Chatbot: " + _get_cleaned_text(event['text']) + "\n")
     return list(reversed(conversation))
