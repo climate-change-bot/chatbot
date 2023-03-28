9221f9ee49eaac0521df7a28460dcccfc3257b3a
roger <climberwoodi@gmx.ch>
2023-03-28 09:16:57 +0200

    Improved the prompt for chatgpt. Removed model instruction because it leads at the moment to strange answers.

--- rasa/actions/action_utils/conversation.py
+++ rasa/actions/action_utils/conversation.py
@@ -2,9 +2,12 @@ from bs4 import BeautifulSoup
 from markdown import markdown
 
 
-def _get_cleaned_text(text):
+def _get_cleaned_text(text, is_chatbot_answer):
     html = markdown(text)
-    return BeautifulSoup(html, features="html.parser").get_text()
+    cleaned_text = BeautifulSoup(html, features="html.parser").get_text()
+    if is_chatbot_answer:
+        cleaned_text = cleaned_text.replace('Herzlich Willkommen :smile:', '')
+    return cleaned_text
 
 
 def _get_text(event):
@@ -13,15 +16,20 @@ def _get_text(event):
     return event['data']['custom']['text']
 
 
+def get_cleaned_chat_gpt_answer(text):
+    text = text.replace('!', '.')
+    return text
+
+
 def get_last_conversation(events, max_number_of_last_conversations):
     events_cleaned = sorted(events, key=lambda x: x['timestamp'], reverse=True)
     events_cleaned = list(filter(lambda x: x['event'] in ['user', 'bot'], events_cleaned))
     conversation = []
     for event in events_cleaned[0:max_number_of_last_conversations]:
         if event['event'] == 'user':
-            content = _get_cleaned_text(_get_text(event))
+            content = _get_cleaned_text(_get_text(event), False)
             if content != '/greet':
                 conversation.append({'role': 'user', 'content': content})
         elif event['event'] == 'bot':
-            conversation.append({'role': 'assistant', 'content': _get_cleaned_text(_get_text(event))})
+            conversation.append({'role': 'assistant', 'content': _get_cleaned_text(_get_text(event), True)})
     return list(reversed(conversation))
--- rasa/actions/action_utils/openai_api_request.py
+++ rasa/actions/action_utils/openai_api_request.py
@@ -1,21 +1,19 @@
 import os
 import openai
 
-from .conversation import get_last_conversation
+from .conversation import get_last_conversation, get_cleaned_chat_gpt_answer
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-MODEL_INSTRUCTION = "Du bist ein freundlicher Chatbot, welcher nur Fragen zum Klimawandel beantwortet."
-
 FIRST_USER_INSTRUCTION = "Beantworte die Frage des user nur, wenn sie mit dem Klimawandel zu tun hat. " \
                          "Ansonsten antworte das du nur Fragen zum Klimawandel beantwortest. " \
-                         "Verwende eine vertraute Anrede (du). Freundliche Sprache. Keine Ausrufezeichen. " \
-                         "Vermeide das Wort bekämpfen. Grüsse den user nicht."
+                         "Verwende eine vertraute Anrede (du) und eine freundliche Sprache. " \
+                         "Vermeide das Wort 'bekämpfen'."
 
 
 def request_to_openai(events):
-    messages = [{'role': 'system', 'content': MODEL_INSTRUCTION}]
-    conversation_items = get_last_conversation(events, 10)
+    messages = []
+    conversation_items = get_last_conversation(events, 7)
     messages.extend(conversation_items)
     messages.insert(-1, {'role': 'user', 'content': FIRST_USER_INSTRUCTION})
 
@@ -23,7 +21,8 @@ def request_to_openai(events):
         model="gpt-3.5-turbo",
         messages=messages,
         max_tokens=400,
-        temperature=0.7
+        temperature=0.8,
+        presence_penalty=0.4
     )
-    response_text = response["choices"][0]['message']['content']
+    response_text = get_cleaned_chat_gpt_answer(response["choices"][0]['message']['content'])
     return response_text
