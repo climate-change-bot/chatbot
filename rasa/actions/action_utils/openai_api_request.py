74542df4e60ad6ea0824715895043aaa4a98c5e6
roger <climberwoodi@gmx.ch>
2023-03-28 11:02:21 +0200

    Added model instruction again to enforce the chatbot to communicate only about climate change.

--- rasa/actions/action_utils/openai_api_request.py
+++ rasa/actions/action_utils/openai_api_request.py
@@ -5,14 +5,17 @@ from .conversation import get_last_conversation, get_cleaned_chat_gpt_answer
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-FIRST_USER_INSTRUCTION = "Beantworte die Frage des user nur, wenn sie mit dem Klimawandel zu tun hat. " \
+MODEL_INSTRUCTION = "Beantworte ausschliesslich Fragen die mit dem Klimawandel zu tun haben."
+
+
+FIRST_USER_INSTRUCTION = "Beantworte die nächste Frage des users nur, wenn sie mit dem Klimawandel zu tun hat. " \
                          "Ansonsten antworte das du nur Fragen zum Klimawandel beantwortest. " \
                          "Verwende eine vertraute Anrede (du) und eine freundliche Sprache. " \
                          "Vermeide das Wort 'bekämpfen'."
 
 
 def request_to_openai(events):
-    messages = []
+    messages = [{'role': 'system', 'content': MODEL_INSTRUCTION}]
     conversation_items = get_last_conversation(events, 7)
     messages.extend(conversation_items)
     messages.insert(-1, {'role': 'user', 'content': FIRST_USER_INSTRUCTION})
@@ -21,8 +24,7 @@ def request_to_openai(events):
         model="gpt-3.5-turbo",
         messages=messages,
         max_tokens=400,
-        temperature=0.8,
-        presence_penalty=0.4
+        temperature=0.7
     )
     response_text = get_cleaned_chat_gpt_answer(response["choices"][0]['message']['content'])
     return response_text
