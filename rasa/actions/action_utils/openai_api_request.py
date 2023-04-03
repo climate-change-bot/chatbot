588523a379b735411fc06d85034c388f001f3061
roger <climberwoodi@gmx.ch>
2023-04-03 14:05:39 +0200

    Added prompt command to engage a user into a discussion.

--- rasa/actions/action_utils/openai_api_request.py
+++ rasa/actions/action_utils/openai_api_request.py
@@ -7,19 +7,20 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 MODEL_INSTRUCTION = "Beantworte ausschliesslich Fragen die mit dem Klimawandel zu tun haben. " \
                     "Versuche mit möglichst wenig Worten die Frage zu beantworten. " \
-                    "Antworte direkt auf die Frage des users ohne ihn zu begrüssen" \
-                    "Frage den User am Schluss ob er weitere Fragen zum Klimawandel hat."
+                    "Stelle am Schluss deiner Antwort eine Frage die den User in eine Gespräch verwickelt."
 
-
-FIRST_USER_INSTRUCTION = "Beantworte die nächste Frage des users nur, wenn sie mit dem Klimawandel zu tun hat. " \
+FIRST_USER_INSTRUCTION = "Beantworte die nächste Frage des users nur, wenn sie mit dem Klimawandel zu tun hat " \
+                         "oder im Kontext der vorherigen Konversation korrekt ist. " \
                          "Ansonsten antworte das du nur Fragen zum Klimawandel beantwortest. " \
+                         "Stelle am Schluss deiner Antwort eine Frage, die den User in ein Gespräch verwickelt. " \
+                         "Zum Beispiel: wie oft isst du Fleisch? Nutzt du bereits den öffentlich Verkehr? usw." \
                          "Verwende eine vertraute Anrede (du) und eine freundliche Sprache. " \
                          "Vermeide das Wort 'bekämpfen' und antworte ohne Umschweife auf die Frage. "
 
 
 def request_to_openai(events):
     messages = [{'role': 'system', 'content': MODEL_INSTRUCTION}]
-    conversation_items = get_last_conversation(events, 7)
+    conversation_items = get_last_conversation(events, 10)
     messages.extend(conversation_items)
     messages.insert(-1, {'role': 'user', 'content': FIRST_USER_INSTRUCTION})
 
